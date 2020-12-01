d1a31bd0477bd1b9c052613ea9daad13e785d84a
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-01 00:51:16 -0500

    Finished day 1

--- day01.rb
+++ day01.rb
@@ -1,8 +1,20 @@
 expenses = File.readlines("input/day01.txt", chomp: true).map do |line|
   line.to_i
 end
+
+# Part 1
 nums = Hash.new
 expenses.each do |e|
-  puts e * (2020-e) if nums.has_key?(2020 - e)
+  puts "The 2 numbers, multiplied, equals #{e * (2020 - e)}." if nums.has_key?(2020 - e)
   nums[e] = 1
+end
+
+# Part 2
+for i in 0..(expenses.length - 3)
+  for j in (i + 1)..(expenses.length - 2)
+    for k in (j + 1)..(expenses.length - 1)
+      sum = expenses[i] + expenses[j] + expenses[k]
+      puts expenses[i] * expenses[j] * expenses[k] if sum == 2020
+    end
+  end
 end
