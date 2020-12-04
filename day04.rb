c75bde73b3d27ded5f8730221a043ac0c1ea240b
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-04 11:05:11 -0500

    Day 4, part 2

--- day04.rb
+++ day04.rb
@@ -1,6 +1,7 @@
 input = File.readlines("input/day04.txt", chomp: true)
 input.append("") # my buffer
 
+# all of the required passport fields
 required = {
     byr: 0,
     iyr: 1,
@@ -10,20 +11,71 @@ required = {
     ecl: 5,
     pid: 6,
 }
-valid = 0 # nr of valid passports
-fields = [0, 0, 0, 0, 0, 0, 0] # fields[i] = 1 if the field is present, 0 if not
 
-input.each do |line|
-  # Part 1
-  pairs = line.split(' ')
-  pairs.each do |pair|
-    kv = pair.split(':')
-    fields[required[kv[0].to_sym]] = 1 if required.has_key? kv[0].to_sym
-  end
-  if line.length == 0
-    valid += 1 if fields.inject(:*) == 1
-    fields = [0, 0, 0, 0, 0, 0, 0]
+def is_valid(key, value)
+  case key
+  when "byr"
+    value.to_i >= 1920 && value.to_i <= 2002
+  when "iyr"
+    value.to_i >= 2010 && value.to_i <= 2020
+  when "eyr"
+    value.to_i >= 2020 && value.to_i <= 2030
+  when "hgt"
+    false if value.length < 4 || value.length > 5
+    case value[-2..-1]
+    when "cm"
+      false if value.length < 5
+      height = value[0..2].to_i
+      height >= 150 && height <= 193
+    when "in"
+      false if value.length > 4
+      height = value[0..1].to_i
+      height >= 59 && height <= 76
+    else
+      false
+    end
+  when "hcl"
+    value.match?(/\A#[0-9a-f]{6}\z/)
+  when "ecl"
+    # valid eye colours
+    eye_colour = {
+        amb: true,
+        blu: true,
+        brn: true,
+        gry: true,
+        grn: true,
+        hzl: true,
+        oth: true,
+    }
+    eye_colour.has_key? value.to_sym
+  when "pid"
+    value.match?(/\A[0-9]{9}\z/)
+  else
+    false
   end
 end
 
-puts "Part 1: There are #{valid} valid passports."
+valid1 = 0 # nr of valid passports
+valid2 = 0
+fields1 = [0, 0, 0, 0, 0, 0, 0] # fields[i] = 1 if the field is present, 0 if not
+fields2 = [0, 0, 0, 0, 0, 0, 0]
+
+input.each do |line|
+  pairs = line.split(' ')
+  pairs.each do |pair|
+    kv = pair.split(':')
+    if required.has_key? kv[0].to_sym
+      fields1[required[kv[0].to_sym]] = 1
+      fields2[required[kv[0].to_sym]] = 1 if is_valid(kv[0], kv[1])
+    end
+  end
+  if line.length == 0
+    valid1 += 1 if fields1.inject(:*) == 1
+    valid2 += 1 if fields2.inject(:*) == 1
+    fields1 = [0, 0, 0, 0, 0, 0, 0]
+    fields2 = [0, 0, 0, 0, 0, 0, 0]
+  end
+end
+
+puts "Part 1: There are #{valid1} valid passports."
+puts "Part 2: There are #{valid2} valid passports."
