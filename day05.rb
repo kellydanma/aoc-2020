168d50d88eb6e66284b7316b193919666829bb96
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-05 01:08:22 -0500

    refactor

--- day05.rb
+++ day05.rb
@@ -1,43 +1,36 @@
 input = File.readlines("input/day05.txt", chomp: true)
 
+# A really bad binary search.
+def b_search(arr, iter, lo, hi, char, pos)
+  iter.times do
+    mid = (lo + hi) / 2
+    if arr[pos] == char
+      hi = mid
+    else
+      lo = mid + 1
+    end
+    pos += 1
+  end
+  return lo # I could also return hi, it's the same.
+end
+
 highest_id = -1
 seats = []
 
+# Part 1
 input.each do |line|
-  pos = 0
-  lo = 0
-  hi = 127
-  7.times do
-    mid = (lo + hi) / 2
-    if line[pos] == 'F'
-      hi = mid
-    else
-      lo = mid + 1
-    end
-    pos += 1
-  end
-  row = lo
-  lo = 0
-  hi = 7
-  3.times do
-    mid = (lo + hi) / 2
-    if line[pos] == 'L'
-      hi = mid
-    else
-      lo = mid + 1
-    end
-    pos += 1
-  end
-  id = row * 8 + lo
-  seats.append(id)
+  row = b_search(line, 7, 0, 127, 'F', 0)
+  col = b_search(line, 3, 0, 7, 'L', 7)
+  id = row * 8 + col
   highest_id = id if id > highest_id
+  seats.append(id)
 end
+puts "Part 1: The highest seat ID is #{highest_id}."
 
-puts "Part 1: the highest seat ID is #{highest_id}."
-
+# Part 2
 seats.sort!
-for i in 0..seats.length-2 do
-  if seats[i+1]-seats[i] ==2
-    puts "Part 2: your seat is #{seats[i]+1}."
+for i in 0..seats.length - 2 do
+  if seats[i + 1] - seats[i] == 2
+    puts "Part 2: Your seat is #{seats[i] + 1}."
   end
 end
