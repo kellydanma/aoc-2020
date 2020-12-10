5ac5e01fbf90450b85d59281579e955d36aa9746
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-09 20:54:26 -0500

    Finished day 8

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ My solutions to [Advent of Code](https://adventofcode.com/), whilst trying to le
 5. [Day 5: Binary Boarding](./day05.rb)
 6. [Day 6: Custom Customs](./day06.rb)
 7. [Day 7: Handy Haversacks](./day07.rb)
+8. [Day 8: Handheld Halting](./day08.rb)
--- day08.rb
+++ day08.rb
@@ -1,23 +1,41 @@
 # Part 1
 program = File.readlines('input/day08.txt', chomp: true).map { |line|
   instruction, val = line.split(' ')
-  [instruction.to_s, val.to_i, true]
+  [instruction.to_s, val.to_i, false]
 }.to_a
 
-accumulator = 0
-pos = 0
-
-while program[pos][2] do
-  program[pos][2] = false
-  case program[pos][0]
-  when "nop"
-    pos += 1
-  when "acc"
-    accumulator += program[pos][1]
-    pos += 1
-  when "jmp"
-    pos += program[pos][1]
+def loops?(program)
+  acc = 0
+  pos = 0
+  while pos < program.length() do
+    return true, acc if program[pos][2]
+    program[pos][2] = true
+    case program[pos][0]
+    when "nop"
+      pos += 1
+    when "acc"
+      acc += program[pos][1]
+      pos += 1
+    when "jmp"
+      pos += program[pos][1]
+    end
   end
+  return false, acc
 end
 
-puts "Part 1: accumulator = #{accumulator}"
+puts "Part 1: accumulator = #{loops?(program.map(&:clone))[1]}"
+
+# Part 2
+program.each.with_index do |p, i|
+  new_program = program.map(&:clone)
+  case new_program[i][0]
+  when "nop" then
+    new_program[i][0] = "jmp"
+  when "jmp" then
+    new_program[i][0] = "nop"
+  else
+    next
+  end
+  ans = loops?(new_program)
+  puts "Part 2: accumulator = #{ans[1]}" if !ans[0]
+end
