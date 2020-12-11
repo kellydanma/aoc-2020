42a6e574a26c04bcac42977eacf9e7b8c6b8182b
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-11 02:02:36 -0500

    Completed day 11

--- day11.rb
+++ day11.rb
@@ -20,6 +20,25 @@ def neighbours(layout, pos)
   adjacent
 end
 
+def visible(layout, pos, x_min, x_max, y_min, y_max)
+  occupied = 0
+  directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+  directions.each do |dx, dy|
+    x = pos[0] + dx
+    y = pos[1] + dy
+    while x >= x_min && x <= x_max && y >= y_min && y <= y_max do
+      if layout[x][y] == "."
+        x += dx
+        y += dy
+        next
+      end
+      occupied += layout[x][y] == "#" ? 1 : 0
+      break
+    end
+  end
+  occupied
+end
+
 # Part 1
 l = layout.map(&:dup)
 loop do
@@ -40,3 +59,24 @@ end
 
 puts "Part 1: #{l.join.count("#")} seats are occupied."
 
+# Part 2
+l = layout.map(&:dup)
+w = layout.first.length - 1
+h = layout.length - 1
+loop do
+  copy = l.map(&:dup)
+  (0...layout.length).each do |i|
+    (0...layout.first.length).each do |j|
+      case l[i][j]
+      when "L" then
+        copy[i][j] = "#" if visible(l, [i, j], 0, h, 0, w) == 0
+      when "#" then
+        copy[i][j] = "L" if visible(l, [i, j], 0, h, 0, w) >= 5
+      end
+    end
+  end
+  break if l == copy
+  l = copy
+end
+
+puts "Part 2: #{l.join.count("#")} seats are occupied."
