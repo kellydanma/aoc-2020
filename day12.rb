22613f37c4b619361515c27a6e6eb027fd6fb328
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-12 16:50:49 -0500

    Completed day 12

--- day12.rb
+++ day12.rb
@@ -8,8 +8,14 @@ instructions = File.readlines('input/day12.txt', chomp: true).map { |line|
 def orient(degrees, cur)
   direction = (degrees / 90) % 4
   cur if direction == 0
-  new_directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-  new_directions[(new_directions.find_index(cur) + direction) % 4]
+  directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+  directions[(directions.find_index(cur) + direction) % 4]
+end
+
+def rotate(degrees, wp)
+  directions = [[1, 0, 0, 1], [0, -1, 1, 0], [-1, 0, 0, -1], [0, 1, -1, 0]]
+  dir = directions[(degrees / 90) % 4]
+  [dir[0] * wp[0] + dir[1] * wp[1], dir[2] * wp[0] + dir[3] * wp[1]]
 end
 
 def manhattan_dist(instructions, x, y)
@@ -37,4 +43,30 @@ def manhattan_dist(instructions, x, y)
   x.abs + y.abs
 end
 
+def manhattan_dist2(instructions, x, y)
+  waypoint = [10, 1]
+  instructions.each do |action, value|
+    case action
+    when "N"
+      waypoint[1] += value
+    when "S"
+      waypoint[1] -= value
+    when "E"
+      waypoint[0] += value
+    when "W"
+      waypoint[0] -= value
+    when "L"
+      waypoint = rotate(value, waypoint)
+    when "R"
+      waypoint = rotate(360 - value, waypoint)
+    when "F"
+      dx, dy = waypoint
+      x += value * dx
+      y += value * dy
+    end
+  end
+  x.abs + y.abs
+end
+
 puts "Part 1: The Manhattan distance is #{manhattan_dist(instructions, 0, 0)}"
+puts "Part 2: The Manhattan distance is #{manhattan_dist2(instructions, 0, 0)}"
