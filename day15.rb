29b6a1325ab5e474b783fa5104f608c59ccb2d02
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-15 09:39:14 -0500

    dun

--- day15.rb
+++ day15.rb
@@ -1,18 +1,19 @@
 def store(mem, key, turn)
-  a, b = mem[key]
-  return mem[key] = [nil, turn] if !mem.has_key?(key)
-  return mem[key] = [b, turn]
+  _, b = mem[key]
+  !mem.has_key?(key) ? mem[key] = [nil, turn] : mem[key] = [b, turn]
 end
 
-# Part 1
-mem = {}
-nums = [1, 20, 11, 6, 12, 0]
-nums.each.with_index { |n, i| mem[n] = [nil, i] }
-(nums.length...2020).each do |i|
-  a, b = mem[nums[i - 1]]
-  cur = a == nil ? 0 : b - a
-  nums.append(cur)
-  store(mem, cur, i)
+def play(nums, last_turn)
+  mem = {}
+  nums.each.with_index { |n, i| mem[n] = [nil, i] }
+  (nums.length...last_turn).each do |i|
+    a, b = mem[nums[i - 1]]
+    cur = a == nil ? 0 : b - a
+    nums << cur
+    store(mem, cur, i)
+  end
+  nums.last
 end
 
-puts "Part 1: #{nums.last}"
+puts "Part 1: #{play([1, 20, 11, 6, 12, 0], 2020)}"
+puts "Part 2: #{play([1, 20, 11, 6, 12, 0], 30000000)}"
