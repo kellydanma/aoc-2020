86f6bd8b05e17cb1eff8eff892298314326dd3cb
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-16 11:56:52 -0500

    finally

--- day16.rb
+++ day16.rb
@@ -1,6 +1,7 @@
 data = File.read('input/day16.txt', chomp: true).split("your ticket:")
 ticket_data = data[1].split("nearby tickets:")
 
+# Data processing
 fields = data[0].lines.reverse.drop(1).reverse.map { |line|
   field = line[/(\w+(?: \w+)?)/]
   v = line.scan(/(\d+)/).map { |n| n.first.to_i }
@@ -10,7 +11,11 @@ nearby_tickets = ticket_data[1].lines.drop(1).map { |line|
   v = line.scan(/(\d+)/).map { |n| n.first.to_i }; v }.to_a
 
 def valid_field?(ranges, field)
-  ranges.each { |range| return true if range.cover?(field) }; false
+  ranges.any? { |range| range.cover?(field) }
+end
+
+def valid_fields?(ranges, fields)
+  fields.all? { |f| valid_field?(ranges, f) }
 end
 
 def valid_ticket?(ranges, ticket)
@@ -23,4 +28,19 @@ error_rate = nearby_tickets.flatten.map { |field| valid_field?(ranges, field) ? 
 puts "Part 1: #{error_rate}"
 
 # Part 2
-remaining = nearby_tickets.map { |t| valid_ticket?(ranges, t) }.reject { |t| t == nil } << my_ticket
+remaining = nearby_tickets.reject { |t| valid_ticket?(ranges, t) == nil } << my_ticket
+my_fields = (0...my_ticket.length).map { |i|
+  rem = remaining.transpose[i]
+  fields.map { |field, ranges| valid_fields?(ranges, rem) ? field : [] }
+      .reject { |f| f.empty? } }
+                .map.with_index
+                .sort_by { |i, _| i.count }
+
+(0...my_ticket.length).each do |i|
+  (i + 1...my_ticket.length).each { |f| my_fields[f][0] -= [my_fields[i][0][0]] } if my_fields[i][0].one?
+end
+
+departures = my_fields.map { |field, pos|
+  field.first.include?("departure") ? my_ticket[pos] : 1 }.inject(:*)
+
+puts "Part 2: #{departures}"
