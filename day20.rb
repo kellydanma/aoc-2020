7e94c816d4d16ae9775724c15a378724dbe4b3c8
Kelly Ma <kelly.ma@mail.mcgill.ca>
2020-12-23 15:10:16 -0500

    Day 20, part 1

--- day20.rb
+++ day20.rb
@@ -1,15 +1,19 @@
 @tiles = File.read('input/day20.txt', chomp: true).split("\r\n\r\n").map { |tile|
-  image = tile.lines.map { |t| t.rstrip }
-  nr = image.first[/\d+/].to_i
-  image = image.drop(1).map { |row| row.split("") }
-  [[image.first, image.transpose.first, image.last, image.transpose.last], nr] # up, left, down, right
+  img = tile.lines.map { |t| t.rstrip }
+  nr = img.first[/\d+/].to_i
+  img = img.drop(1)
+  img_trans = img.map { |row| row.split("") }.transpose.map { |row| row.join("") }
+  [[img.first, img_trans.first, img.last, img_trans.last], nr] # up, left, down, right
 }.to_h
+
+ids = @tiles.values
 
 # flip all of the tiles
 existing_tiles = @tiles.keys
 existing_tiles.each do |t|
   up, left, down, right = t
   @tiles[[up.reverse, right, down.reverse, left]] = @tiles[t]
+  @tiles[[down, left.reverse, up, right.reverse]] = @tiles[t]
 end
 
 # rotate all of the tiles
@@ -20,3 +24,10 @@ existing_tiles.each do |t|
   @tiles[[down.reverse, right.reverse, up.reverse, left.reverse]] = @tiles[t]
   @tiles[[left.reverse, down, right.reverse, up.reverse]] = @tiles[t]
 end
+
+# Part 1
+@borders = @tiles.keys.transpose
+unique = Hash.new(0)
+@tiles.each { |bd, id| unique[id] += bd.map.with_index { |b, n| @borders[n].one?(b) ? 1 : 0 }.sum }
+part1 = unique.select { |_, n| n == 12 }.map { |id, _| id }
+puts "Part 1: #{part1.inject(:*)}"
